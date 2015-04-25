82612097fcc076abe44c76f4779bc9d378b7a1c3
gorkabarturen <gorka.barturen@opendeusto.es>
2015-04-25 13:06:19 +0200

    Modulo elemento

---
+++ Clepizza/Elemento.c
@@ -0,0 +1,19 @@
+/*
+ * Elemento.c
+ *
+ *  Created on: 25/4/2015
+ *      Author: gorka
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void iniciarElementos ()
+{
+
+}
+
+void mostrarElemento()
+{
+
+}
