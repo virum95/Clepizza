549cf4b8085cf7a99c6bb7fe0b30fb76b7eea9e8
gorkabarturen <gorka.barturen@opendeusto.es>
2015-04-27 10:15:16 +0200

    asd

--- Clepizza/Elemento.c
+++ Clepizza/Elemento.c
@@ -10,82 +10,55 @@
 #include <string.h>
 #include "Elemento.h"
 
-void handle_line(char *line, Elemento* e) {
-	strcpy(e->nombre, line);
-	//Jorca tonto
+void handle_line(char *line, Elemento e) {
+	strcpy(e.nombre, line);
 }
 
-int readFile(FILE* f, Elemento* e) {
-	int size = 1024, pos;
-	int c;
-	char *buffer = (char *)malloc(size);
-	if(f) {
-		do { // read all lines in file
-			pos = 0;
-			do{ // read one line
-				c = fgetc(f);
-				if(c != EOF) buffer[pos++] = (char)c;
-				if(pos >= size - 1) { // increase buffer length - leave room for 0
-					size *=2;
-					buffer = (char*)realloc(buffer, size);
-				}
-			}while(c != EOF && c != '\n');
-			buffer[pos] = 0;
-			// line is now in buffer
-			handle_line(buffer, e);
-		} while(c != EOF);
-		fclose(f);
-	}
-	free(buffer);
-	return 0;
+
+int readLine(FILE* f, Elemento* e) {
+	int pos=0;
+	int i;
+	char c;
+
+	for(i = 0; i<10; i++)
+	{
+	    while(c != EOF && c != (char)(2+i))
+	    {
+	         c = fgetc(f);
+	         if(c != EOF)
+	         {
+	        	 e[i].nombre[pos] = c;
+	        	 //printf("LETRA: %i, %c",c, i+2);
+	        	 fflush(stdout);
+	          	 pos++;
+	         }
+	     }
+	    pos = 0;
+	    }
+	    fclose(f);
+
+	    return 0;
 }
 
 void iniciarElementos (Elemento** arrayElementos, int size )
 {
 	int i;
-	int contadorTipo = 1;
-	for(contadorTipo = 1;contadorTipo<=size;contadorTipo++)
+	int j;
+	for(i = 1; i<=size; i++)
 	{
-		if(contadorTipo == 1)
+		for(j = 0; j<10; j++)
 		{
-			for(i = 0; i<10; i++)
-			{
-			readFile(fopen("Pizzas.txt", "r"), &arrayElementos[0]);
-			arrayElementos[contadorTipo-1]->tipo = contadorTipo;
-			arrayElementos[contadorTipo-1]++;
-			}
-		}
-		if(contadorTipo == 2)
-		{
-			for(i = 0; i<10; i++)
-			{
-			readFile(fopen("Entrantes.txt", "r"), &arrayElementos[0]);
-			arrayElementos[contadorTipo-1]->tipo = contadorTipo;
-			arrayElementos[contadorTipo-1]++;
-			}
-		}
-		if(contadorTipo == 3)
-		{
-			for(i = 0; i<10; i++)
-			{
-			readFile(fopen("Bebidas.txt", "r"), &arrayElementos[0]);
-			arrayElementos[contadorTipo-1]->tipo = contadorTipo;
-			arrayElementos[contadorTipo-1]++;
-			}
-		}
-		if(contadorTipo == 4)
-		{
-			for(i = 0; i<10; i++)
-			{
-			readFile(fopen("Postres.txt", "r"), &arrayElementos[0]);
-			arrayElementos[contadorTipo-1]->tipo = contadorTipo;
-			arrayElementos[contadorTipo-1]++;
-			}
+			arrayElementos[i][j].tipo = i;
+			arrayElementos[i][j].nombre = (char*) malloc(sizeof(char)*20);
+
 		}
 	}
+	readLine(fopen("Pizzas.txt","r"), arrayElementos[0]);
+	//arrayElementos[0][0].nombre="ASDASD";
 }
 
 void mostrarElemento(Elemento e)
 {
 	printf("%s",e.nombre);
+	fflush(stdout);
 }
